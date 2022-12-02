3cdefa6b9ce08292cd42dbd1e788c1511f31b37f
farisrfp <farisrafi2903@gmail.com>
2022-12-02 10:51:53 +0700

    feat: i2c ke ds1307

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,43 @@
-#include <Arduino.h>
+#define DS1307_I2C_ADDRESS 0x68
 
-/* Pin Def */
+#include "Arduino.h"
+#include "Wire.h"
 
+typedef struct DateTime {
+  // byte = uint8_t
+  byte second;
+  byte minute;
+  byte hour;
+  byte dayOfWeek;
+  byte dayOfMonth;
+  byte month;
+  byte year;
+} DateTime_t;
+  
 
-/* Global Var */
+/* Include the following functions in your sketch */
+#include "Function.h"
 
-
-void setup() {
-  Serial.begin(115200);
-  delay(500); // Wait for Serial Monitor
-
+void setup()
+{
+  /* Initialize serial communication and wait for port to open: */
+  Wire.begin();         // Join I2C Bus (address optional for master)
+  Serial.begin(115200); // Start the Serial communication to send messages to the computer
+  delay(1000);          // Wait for 1 second
 }
 
-void loop() {
+void loop()
+{
+  /* Initialize local variables */
+  DateTime_t waktu;
+  bool err;
+  
+  /* Read */
+  err = getDateTime(&waktu);
 
+  /* Debug  */
+
+  /* Output */
+  Serial.println("Hello World!");
+  Serial.println(waktu.second);
 }
